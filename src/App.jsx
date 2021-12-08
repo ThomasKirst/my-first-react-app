dabec5b7d81289c1f43841918a5999531f9eaba8
Thomas Kirst <thomas@moneygarden.de>
2021-12-08 21:40:42 +0100

    Extracted todo and made adding a todo possible

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,52 @@
+import { useState } from 'react';
 import './App.css';
 import Button from './Button';
 import Headline from './Headline';
-
-function Todo({ color, title }) {
-  return <li style={{ color: color }}>{title}</li>;
-}
+import Todo from './Todo';
 
 function App() {
+  let initialTodos = [
+    { title: 'Milch kaufen', color: 'yellow' }, // 0
+    { title: 'Wohnung putzen', color: 'blue' }, // 1
+    { title: 'React Doku lesen', color: 'green' },
+    { title: 'Katze füttern', color: 'red' },
+    { title: 'Glühwein im Glühwein Express trinken', color: 'red' },
+  ];
+
+  const [todos, setTodos] = useState(initialTodos);
+
+  const todoItems = todos.map((todo, index) => (
+    <Todo key={index} title={todo.title} color={todo.color} />
+  ));
+
+  function addTodo(event) {
+    // Formular nicht abschicken (Kein Server, der die Daten aufnimmt, mir machen etwas lokal (im Browser) damit)
+    event.preventDefault();
+
+    // Event wurde im Formular ausgelöst, event.target (auslösende Element)
+    const form = event.target;
+    const inputField = form.color;
+    const inputValue = inputField.value;
+
+    // deklariert und initialisiert mit einem Objekt
+    const newTodo = {
+      title: inputValue,
+      color: 'gray',
+    };
+
+    setTodos([...todos, newTodo]);
+  }
+
   return (
     <div className="App">
       <Headline title="I am your shopping list" />
 
-      <ul>
-        <Todo title="Milch kaufen" color="yellow" />
-        <Todo title="Wohnung putzen" color="blue" />
-        <Todo title="React Doku lesen" color="green" />
-        <Todo title="Katze füttern" color="red" />
-      </ul>
+      <form onSubmit={addTodo}>
+        <input type="text" name="todo" placeholder="Add a todo …" />
+        <Button text="Submit" />
+      </form>
 
-      <Button text="Click me!" />
-      <Button isLarge text="I am the second button" />
+      <ul>{todoItems}</ul>
     </div>
   );
 }
---
+++ src/Todo.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './Todo.css';
+
+// Wird für jedes einzelne Todo ausgeführt
+function Todo({ color, title }) {
+  const [isDone, setIsDone] = useState(false); // initial: isDone = false
+
+  function markAsDone() {
+    // Zustandsänderung, ausgelöst durch den Checkbox-Klick (onChange)
+    // Ursprungszustand -> Zielzustand (wie und wann entscheidet React)
+    setIsDone(isDone ? false : true); //
+    // State Change ? Flip da hook -> Re-render wird ausgelöst!
+  }
+
+  // isDone ist ein bool'scher Wert,
+  // er muss nicht explizit mit === true verglichen werden
+  const listItemLook = isDone ? 'strike-through' : '';
+
+  return (
+    <li style={{ color: color }}>
+      <label className={listItemLook}>
+        <input type="checkbox" onChange={markAsDone} /> {title}
+      </label>
+    </li>
+  );
+}
+
+export default Todo;
